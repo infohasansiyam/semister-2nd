ea96066e3eadc31fadba7e730da46d85473a1851
infohasansiyam <infohasansiyam@gmail.com>
2022-12-22 11:20:19 +0600

    new

--- main.js
+++ main.js
@@ -7,16 +7,26 @@
 
 
 // }
+function mathMatic(){
+    let Sum1=10;
+    let Sum2=20;
+    let Sum3=30;
 
-
-// Parametar function
-function mathMatic(Num1,Num2,Num3){
-    document.write("<br/> The Addition: "+(Num1+Num2+Num3));
+    document.write("<br/> The Addition: "+(Sum1+Sum2+Sum3));
     document.write("<br/> The Subtraction: "+(Num3+Num2+Num1));
     document.write("<br/> The Multiplication: "+(Num1*Num2*Num3));
     document.write("<br/> The divition: "+((Num3/Num2)/Num1));
 }
-let N1=Number (prompt("Enter a Number: "));
-let N2=Number (prompt("Enter a Number: "));
-let N3=Number (prompt("Enter a Number: "));
-mathMatic(N1,N2,N3);
+mathMatic();
+
+// Parametar function
+// function mathMatic(Num1,Num2,Num3){
+//     document.write("<br/> The Addition: "+(Num1+Num2+Num3));
+//     document.write("<br/> The Subtraction: "+(Num3+Num2+Num1));
+//     document.write("<br/> The Multiplication: "+(Num1*Num2*Num3));
+//     document.write("<br/> The divition: "+((Num3/Num2)/Num1));
+// }
+// let N1=Number (prompt("Enter a Number: "));
+// let N2=Number (prompt("Enter a Number: "));
+// let N3=Number (prompt("Enter a Number: "));
+// mathMatic(N1,N2,N3);
