e67f84ac6316e2239d3559f7b8c8d067b8d8aa1d
infohasansiyam <infohasansiyam@gmail.com>
2022-12-22 12:09:51 +0600

    User name code

--- main.js
+++ main.js
@@ -3,30 +3,38 @@
 // 
 // let Num1=Number(prompt("Enter a number: "));
 // function mathMatic(){
-// document.write(Num1,)
+// document.write(Num1,) }
 
+// function mathMatic(){
+//     let Sum1=10;
+//     let Sum2=20;
+//     let Sum3=30;
 
+//     document.write("<br/> The Addition: "+(Sum1+Sum2+Sum3));
+//     document.write("<br/> The Subtraction: "+(Num3+Num2+Num1));
+//     document.write("<br/> The Multiplication: "+(Num1*Num2*Num3));
+//     document.write("<br/> The divition: "+((Num3/Num2)/Num1));
 // }
-function mathMatic(){
-    let Sum1=10;
-    let Sum2=20;
-    let Sum3=30;
+// mathMatic();
 
-    document.write("<br/> The Addition: "+(Sum1+Sum2+Sum3));
-    document.write("<br/> The Subtraction: "+(Num3+Num2+Num1));
-    document.write("<br/> The Multiplication: "+(Num1*Num2*Num3));
-    document.write("<br/> The divition: "+((Num3/Num2)/Num1));
-}
-mathMatic();
+// // Parametar function
+// function mathMatic(Num1,Num2,Num3){
+//     document.write("<br/> The Addition: "+(Num1+Num2+Num3));
+//     document.write("<br/> The Subtraction: "+(Num3+Num2+Num1));
+//     document.write("<br/> The Multiplication: "+(Num1*Num2*Num3));
+//     document.write("<br/> The divition: "+((Num3/Num2)/Num1));
+// }
+// let N1=Number (prompt("Enter a Number: "));
+// let N2=Number (prompt("Enter a Number: "));
+// let N3=Number (prompt("Enter a Number: "));
+// mathMatic(N1,N2,N3);
 
-Parametar function
-function mathMatic(Num1,Num2,Num3){
-    document.write("<br/> The Addition: "+(Num1+Num2+Num3));
-    document.write("<br/> The Subtraction: "+(Num3+Num2+Num1));
-    document.write("<br/> The Multiplication: "+(Num1*Num2*Num3));
-    document.write("<br/> The divition: "+((Num3/Num2)/Num1));
+function math(user,pass){
+        
+    document.write("<br/> Username is : "+ user)
+    document.write("<br/> User's Password : "+ pass)
+
 }
-let N1=Number (prompt("Enter a Number: "));
-let N2=Number (prompt("Enter a Number: "));
-let N3=Number (prompt("Enter a Number: "));
-mathMatic(N1,N2,N3);
+let user=prompt("Enter user name");
+let pass=prompt("Enter your Password");
+math(user,pass)
