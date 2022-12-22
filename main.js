5956ba062690e16435911646a2019b903f18a891
infohasansiyam <infohasansiyam@gmail.com>
2022-12-22 11:30:41 +0600

    New

--- main.js
+++ main.js
@@ -19,14 +19,14 @@ function mathMatic(){
 }
 mathMatic();
 
-// Parametar function
-// function mathMatic(Num1,Num2,Num3){
-//     document.write("<br/> The Addition: "+(Num1+Num2+Num3));
-//     document.write("<br/> The Subtraction: "+(Num3+Num2+Num1));
-//     document.write("<br/> The Multiplication: "+(Num1*Num2*Num3));
-//     document.write("<br/> The divition: "+((Num3/Num2)/Num1));
-// }
-// let N1=Number (prompt("Enter a Number: "));
-// let N2=Number (prompt("Enter a Number: "));
-// let N3=Number (prompt("Enter a Number: "));
-// mathMatic(N1,N2,N3);
+Parametar function
+function mathMatic(Num1,Num2,Num3){
+    document.write("<br/> The Addition: "+(Num1+Num2+Num3));
+    document.write("<br/> The Subtraction: "+(Num3+Num2+Num1));
+    document.write("<br/> The Multiplication: "+(Num1*Num2*Num3));
+    document.write("<br/> The divition: "+((Num3/Num2)/Num1));
+}
+let N1=Number (prompt("Enter a Number: "));
+let N2=Number (prompt("Enter a Number: "));
+let N3=Number (prompt("Enter a Number: "));
+mathMatic(N1,N2,N3);
